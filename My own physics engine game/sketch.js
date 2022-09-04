0b28230d138a71854163bc40dfc8efc47c7b6dd8
zafeerhirfan <94226398+zafeerhirfan@users.noreply.github.com>
2022-09-04 09:31:10 +0530

    Update sketch.js

--- My own physics engine game/sketch.js
+++ My own physics engine game/sketch.js
@@ -1,19 +1,1 @@
-const Engine = Matter.Engine;
-const Render = Matter.Render;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-const Body = Matter.Body;
-const Composites = Matter.Composites;
-const Composite = Matter.Composite;
 
-
-let engine;
-let world;
-
-function preload(){}
-
-function draw(){
-
-
-} 
